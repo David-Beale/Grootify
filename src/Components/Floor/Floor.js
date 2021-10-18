20eefcba319c96a966d4967848de4262f255dbbe
David B <59053870+David-Beale@users.noreply.github.com>
2021-10-18 20:34:59 +0100

    Update Floor.js

--- src/Components/Floor/Floor.js
+++ src/Components/Floor/Floor.js
@@ -2,7 +2,7 @@ export default function Floor() {
   return (
     <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -20, 0]} receiveShadow>
       <circleBufferGeometry args={[75, 32]} />
-      <meshStandardMaterial color="rgb(0, 37, 46)" />
+      <meshStandardMaterial color="rgb(0, 67, 78)" />
     </mesh>
   );
 }
