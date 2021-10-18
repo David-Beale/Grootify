5a870931a2e371d839adbb077ecbdc77d5729fc5
David B <59053870+David-Beale@users.noreply.github.com>
2021-10-18 20:30:33 +0100

    add rig

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import { Suspense, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { Loader, OrbitControls, Stats } from "@react-three/drei";
 import Main from "./Components/Main/Main";
+import { useMouseInteraction } from "./Components/Rig/useInteractions";
+import Rig from "./Components/Rig/Rig";
 
 export default function App() {
   const [clicked, setClicked] = useState(false);
   const onClick = () => {
     setClicked([]);
   };
+
+  const [mouse, onMouseMove] = useMouseInteraction();
   return (
-    <div className="container" onClick={onClick}>
+    <div className="container" onClick={onClick} onPointerMove={onMouseMove}>
       <Suspense fallback={null}>
         <Canvas
           camera={{
@@ -22,7 +26,8 @@ export default function App() {
         >
           <Stats className="stats" />
           <Main clicked={clicked} />
-          <OrbitControls />
+          {/* <OrbitControls /> */}
+          <Rig mouse={mouse} />
         </Canvas>
       </Suspense>
       <Loader />
---
+++ src/Components/Rig/useInteractions.js
@@ -0,0 +1,14 @@
+import { useCallback, useRef } from "react";
+
+export const useMouseInteraction = () => {
+  const mouse = useRef([0.5, 0.5]);
+
+  const onMouseMove = useCallback((e) => {
+    mouse.current = [
+      2500 * (e.clientX / window.innerWidth - 0.5),
+      1500 * (e.clientY / window.innerHeight - 0.5),
+    ];
+  }, []);
+
+  return [mouse, onMouseMove];
+};
