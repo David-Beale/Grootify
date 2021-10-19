2d689765d5534e0500b5321fcda83da5c0268ab7
David B <59053870+David-Beale@users.noreply.github.com>
2021-10-19 20:20:04 +0100

    add custom spotlight

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,4 +1,4 @@
-import { SpotLight } from "@react-three/drei";
+import { SpotLight } from "../SpotLight/SpotLight";
 import { useCallback, useRef, useState } from "react";
 import Floor from "../Floor/Floor";
 import Logo from "../Logo/Logo";
@@ -15,25 +15,27 @@ export default function Main({ clicked }) {
   const modelRef = useRef();
 
   const lightsOn = useCallback(() => {
-    lightRef.current.target = modelRef.current;
     lightRef.current.intensity = 5;
+    lightRef.current.angle = 0.8;
     bloomRef.current.strength = 4;
-    setAngle(3);
+    setAngle(0.8);
   }, []);
 
   return (
     <>
       <NoEffects>
         <Model clicked={clicked} setLightOn={lightsOn} modelRef={modelRef} />
-        <ambientLight intensity={0.5} />
+        <ambientLight intensity={0.2} />
         <SpotLight
           ref={lightRef}
           intensity={0}
           position={[0, 40, 0]}
-          distance={80}
           angle={angle}
+          penumbra={1}
+          distance={150}
           attenuation={75}
           anglePower={5}
+          target={modelRef.current}
           color="white"
           castShadow
         />
