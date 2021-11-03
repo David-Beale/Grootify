2bb9f196b6a635787baf9ebab9bb41172665193e
David B <59053870+David-Beale@users.noreply.github.com>
2021-11-03 14:10:50 +0000

    Update Ring.js

--- src/Components/Ring/Ring.js
+++ src/Components/Ring/Ring.js
@@ -1,7 +1,7 @@
 export default function Ring() {
   return (
     <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -20, 0]}>
-      <torusBufferGeometry args={[76, 1, 3, 36]} />
+      <torusBufferGeometry args={[76, 1, 3, 36, Math.PI]} />
       <meshBasicMaterial color="chartreuse" />
     </mesh>
   );
