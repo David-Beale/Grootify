152a340c6e9f1f4daa8e862c17ea6bd7c2cd10d6
David B <59053870+David-Beale@users.noreply.github.com>
2021-11-04 19:10:07 +0000

    fix mouse rig

--- src/Components/Main/Rig/useInteractions.js
+++ src/Components/Main/Rig/useInteractions.js
@@ -2,18 +2,18 @@ import { useCallback, useRef } from "react";
 import { useStore } from "../../Store/store";
 
 export const useMouseInteraction = () => {
-  const loggedIn = useStore((state) => state.loggedIn);
+  const mode = useStore((state) => state.mode);
   const mouse = useRef([0, 0]);
 
   const onMouseMove = useCallback(
     (e) => {
-      if (!loggedIn) return;
+      if (!mode) return;
       mouse.current = [
         e.clientX / window.innerWidth - 0.5,
         e.clientY / window.innerHeight - 0.5,
       ];
     },
-    [loggedIn]
+    [mode]
   );
 
   return [mouse, onMouseMove];
